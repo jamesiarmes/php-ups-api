e8dac9ce3035157d9a026164fa16e5cc9f10876d
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2008-01-12 02:26:57 +0000

    Updated Config Template and added UpsAPI Class

---
+++ trunk/UpsAPI.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * PHP API for use with UPS OnLine Tools.  This is the main class that
+ * all other classes will extend.
+ *
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+ 
+/**
+ * Include the configuration file
+ */
+require_once 'config.php';
+
+/**
+ * Parent class for the UpsAPI
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+class UpsAPI {
+	/**
+	 * Access key provided by UPS
+	 * 
+	 * @param string
+	 * @access protected
+	 */
+	protected $access_key;
+	
+	/**
+	 * Developer key provided by UPS
+	 * 
+	 * @param string
+	 * @access protected
+	 */
+	protected $developer_key;
+	
+	/**
+	 * Password used to access UPS Systems
+	 * 
+	 * @param string
+	 * @access protected
+	 */
+	protected $password;
+	
+	/**
+	 * Username used to access UPS Systems
+	 * 
+	 * @param string
+	 * @access protected
+	 */
+	protected $username;
+	
+	/**
+	 * Sets up the API Object
+	 * 
+	 * @access public
+	 */
+	public function __construct() {
+		/** Set the Keys on the Object **/
+		$this->access_key = $GLOBALS['ups_api']['access_key'];
+		$this->developer_key = $GLOBALS['ups_api']['developer_key'];
+		
+		
+		/** Set the username and password on the Object **/
+		$this->password = $GLOBALS['ups_api']['password'];
+		$this->username = $GLOBALS['ups_api']['username'];
+	} // end funciton __construct()
+} // end class UpsAPI
+
+?>
--- trunk/config_template.php
+++ trunk/config_template.php
@@ -1,5 +1,10 @@
 <?php
-/*** set the developer and access keys **/
+/** set the developer and access keys **/
 $GLOBALS['ups_api']['access_key'] = '';
 $GLOBALS['ups_api']['developer_key'] = '';
+
+
+/** set the username and password used to connect to UPS **/
+$GLOBALS['ups_api']['username'] = '';
+$GLOBALS['ups_api']['password'] = '';
 ?>
