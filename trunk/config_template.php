cffa006668488a846f1606cecbfdcbf8688485ff
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2008-01-14 03:29:26 +0000

    Added server to GLOBAL variables for the API

--- trunk/config_template.php
+++ trunk/config_template.php
@@ -2,6 +2,7 @@
 /** set the developer and access keys **/
 $GLOBALS['ups_api']['access_key'] = '';
 $GLOBALS['ups_api']['developer_key'] = '';
+$GLOBALS['ups_api']['server'] = '';
 
 
 /** set the username and password used to connect to UPS **/
