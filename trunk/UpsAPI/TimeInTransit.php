15a411890eb43a668d2574253d54bb528b0dc046
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2008-03-05 03:05:30 +0000

    Removed todo from address validation
    initial implementation of time in transit

--- trunk/UpsAPI/TimeInTransit.php
+++ trunk/UpsAPI/TimeInTransit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Handles the validation of US Shipping Addresses
+ * Get shipping time based on origin and destination address
  * 
  * Copyright (c) 2008, James I. Armes
  * All rights reserved.
@@ -29,15 +29,16 @@
  * 
  * @author James I. Armes <jamesiarmes@gmail.com>
  * @package php_ups_api
+ * @todo Implement
  */
 
 /**
- * Handles the validation of US Shipping Addresses
+ * Get shipping time based on origin and destination address
  * 
  * @author James I. Armes <jamesiarmes@gmail.com>
  * @package php_ups_api
  */
-class UpsAPI_USAddressValidation extends UpsAPI {
+class UpsAPI_TimeInTransit extends UpsAPI {
 	/**
 	 * Shipping address that we are to validate
 	 * 
@@ -59,100 +60,6 @@ class UpsAPI_USAddressValidation extends UpsAPI {
 		$this->server = $GLOBALS['ups_api']['server'].'/ups.app/xml/AV';
 		$this->address = $address;
 	} // end function __construct()
-	
-	/**
-	 * Gets the current city on the object
-	 *	
-	 * @access public
-	 * @return string the current city
-	 */
-	public function getCity()
-	{
-		return $this->address['city'];
-	} // end function getCity()
-	
-	/**
-	 * Sets the city on the object
-	 * 
-	 * @access public
-	 * @param string $city city to set on the object
-	 */
-	public function setCity($city)
-	{
-		$this->address['city'] = $city;
-		
-		return true;
-	} // end function setCity()
-	
-	/**
-	 * Gets the current full address on the object
-	 * 
-	 * @access public
-	 * @return array the current address
-	 */
-	public function getFullAddress()
-	{
-		return $this->address;
-	} // end function getFullAddress()
-	
-	/**
-	 * Sets the full address on the object
-	 * 
-	 * @access public
-	 * @param array $address address to set on the object
-	 */
-	public function setFullAddress($address)
-	{
-		$this->address = $address;
-		
-		return true;
-	} // end function setFullAddress()
-	
-	/**
-	 * Gets the current state on the object
-	 * 
-	 * @access public
-	 * @return string the current state
-	 */
-	public function getState()
-	{
-		return $this->address['state'];
-	} // end function getState()
-	
-	/**
-	 * Sets the state on the object
-	 * 
-	 * @access public
-	 * @param string $state state to set on the object
-	 */
-	public function setState($state)
-	{
-		$this->address['state'] = $state;
-		
-		return true;
-	} // end function setState()
-	
-	/**
-	 * Gets the current zip code on the object
-	 * 
-	 * @access public
-	 * @return integer the curret zip code
-	 */
-	public function getZipCode()
-	{
-		return $this->address['zip_code'];
-	} // end function getZipCode()
-	
-	/**
-	 * Sets the zip code on the object
-	 * 
-	 * @access public
-	 * @param integer $zip_code zip code to set on the object
-	 */
-	public function setZipCode($zip_code)
-	{
-		$this->address['zip_code'] = $zip_code;
-	} // end function setZipCode()
 	
 	/**
 	 * Builds the XML used to make the request
@@ -252,64 +159,6 @@ class UpsAPI_USAddressValidation extends UpsAPI {
 	} // end function buildRequest()
 	
 	/**
-	 * Gets any matches from the response including their rank and quality
-	 * 
-	 * @access public
-	 * @param string $match_type type of match returned by getMatchType()
-	 * @return array $return_value array of matches
-	 */
-	public function getMatches($match_type = null)
-	{
-		$return_value = array();
-		
-		// check if a valid match type was passed in
-		$valid_match = $this->validateMatchType($match_type);
-		if (empty($match_type))
-		{
-			$match_type = $this->getMatchType();
-		} // end if no valid match type was passed in
-		
-		// check if there are any matches
-		if ($match_type == 'None')
-		{
-			return $return_value;
-		} // end if there are not matches
-		
-		// check if we only have one match
-		$match_array = $this->response_array['AddressValidationResult'];
-		if (!isset($match_array[0]))
-		{
-			$return_value[0] = array(
-				'quality' => $match_array['Quality'],
-				'address' => array(
-					'city' => $match_array['Address']['City'],
-					'state' => $match_array['Address']['StateProvinceCode'],
-				),
-				'zip_code_low' => $match_array['PostalCodeLowEnd'],
-				'zip_code_high' => $match_array['PostalCodeHighEnd'],
-			); // end $return_value
-		} // end if we only have one match
-		else
-		{
-			// iterate over the matches
-			foreach ($match_array as $current_match)
-			{
-				$return_value[] = array(
-					'quality' => $current_match['Quality'],
-					'address' => array(
-						'city' => $current_match['Address']['City'],
-						'state' => $current_match['Address']['StateProvinceCode'],
-					),
-					'zip_code_low' => $current_match['PostalCodeLowEnd'],
-					'zip_code_high' => $current_match['PostalCodeHighEnd'],
-				); // end $return_value
-			} // end for each match
-		} // end if we have multiple matches
-		
-		return $return_value;
-	} // end function getMatches()
-	
-	/**
 	 * Returns the type of match(s)
 	 * 
 	 * @access public
@@ -361,22 +210,6 @@ class UpsAPI_USAddressValidation extends UpsAPI {
 		
 		return $return_value;
 	} // end function getMatchType()
-	
-	/**
-	 * Checks a match type to see if it is valid
-	 * 
-	 * @access protected
-	 * @param string $match_type match type to validate
-	 * @return bool whether or not the match type is valid
-	 */
-	protected function validateMatchType($match_type)
-	{
-		// declare the valid match types
-		$valid_match_types = array('Exact', 'None', 'Partial',
-			'Multiple With Exact', 'Multiple Partial');
-		
-		return in_array($match_type, $valid_match_types);
-	} // end function validateMatchType()
-} // end class UpsAPI_USAddressValidation
+} // end class UpsAPI_TimeInTransit
 
 ?>
