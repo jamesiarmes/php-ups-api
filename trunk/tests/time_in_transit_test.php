915470bfb066667675287dd66108829fa99282dc
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2008-04-19 01:20:24 +0000

    Updated Time In Transit Test

--- trunk/tests/time_in_transit_test.php
+++ trunk/tests/time_in_transit_test.php
@@ -26,7 +26,19 @@ if (!empty($_POST['submit']))
 			'state' => $_POST['destination_state'],
 			'zip_code' => $_POST['destination_zip_code'],
 			),
-		'pickup_date' => '',
+		'pickup_date' => $_POST['pickup_date'],
+		'max_list_size' => $_POST['max_list_size'],
+		'invoice' => array(
+			'currency_code' => $_POST['currency_code'],
+			'monetary_value' => $_POST['monetary_value'],
+		),
+		'weight' => array(
+			'unit_of_measure' => array(
+				'code' => $_POST['weight_um'],
+				'desc' => $_POST['weight_desc'],
+			),
+			'weight' => $_POST['weight'],
+		),
 	); // end $data
 	
 	$validation = new UpsAPI_TimeInTransit($data);
@@ -124,6 +136,72 @@ else
 			size="2" maxlength="2" value="PA" />
 		<input type="text" name="destination_zip_code" id="destination_zip_code"
 			size="5" maxlength="5" value="17011" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="origin_country">Country: </label>
+	</td>
+	<td>
+		<input type="text" name="origin_country" id="origin_country" size="2"
+			maxlength="2" value="US" />
+	</td>
+	<td>
+		<input type="text" name="destination_country" id="destination_country"
+			size="2" maxlength="2" value="US" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="pickup_date">Pickup Date: </label>
+	</td>
+	<td colspan="2">
+		<input type="text" name="pickup_date" id="pickup_date" size="8"
+			maxlength="8" value="20080421" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="max_list_size">Maximum List Size (1-50): </label>
+	</td>
+	<td colspan="2">
+		<input type="text" name="max_list_size" id="max_list_size" size="2"
+			maxlength="2" value="35" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="currency_code">Currency/Amount: </label>
+	</td>
+	<td>
+		<input type="text" name="currency_code" id="currency_code" size="3"
+			maxlength="3" value="USD" />
+	</td>
+	<td>
+		<input type="text" name="monetary_value" id="monetary_value" size="11"
+			maxlength="11" value="500.00" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="weight_um">Weight UM Appr./Desc.: </label>
+	</td>
+	<td>
+		<input type="text" name="weight_um" id="weight_um" size="3"
+			maxlength="3" value="LBS" />
+	</td>
+	<td>
+		<input type="text" name="weight_desc" id="weight_desc" size="25"
+			maxlength="255" value="Pounds" />
+	</td>
+</tr>
+<tr>
+	<td>
+		<label for="weight">Weight: </label>
+	</td>
+	<td colspan="2">
+		<input type="text" name="weight" id="weight" size="3"
+			maxlength="3" value="23" />
 	</td>
 </tr>
 <tr>
