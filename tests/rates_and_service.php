4f20cba06176390dfa802433dc4e8f105c65729b
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2008-12-14 16:12:50 +0000

    Rates and service documentation

---
+++ tests/rates_and_service.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Include the configuration file
+ */
+require_once '../inc/config.php';
+
+echo '<img src="ups_logo.gif" /><br />';
+
+// check if the form was submitted
+if (!empty($_POST['submit']))
+{
+} // end if the form has been submitted
+else
+{
+?>
+<?php
+} // end else the form has not been submitted
+
+?>
