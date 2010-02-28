8d1af206756e7045f04037aaa682a67053d6d677
JamesIArmes <JamesIArmes@c2f73356-6743-0410-8b3b-4393789b6797>
2010-02-28 00:59:51 +0000

    Added types for the different package service options

---
+++ branches/0.x/UpsType/PackageServiceOptions.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * UPS Address Type.  Makes handling of package service options.
+ * 
+ * Copyright (c) 2008, James I. Armes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+
+/**
+ * UPS Address Type.  Makes handling of package service options.
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+class UpsType_PackageServiceOptions extends UpsType {
+	/**
+	 * Array of service options
+	 * 
+	 * @var array
+	 */
+	protected $options = array();
+	
+	/**
+	 * Constructor. Values may be passed here or by calling the individual set
+	 * functions.
+	 * 
+	 * @param array $options
+	 */
+	public function __construct($options = array()) {
+		// set any of the values that were passed in
+		$this->options = $options;
+	} // end function __construct()
+	
+	/**
+	 * Returns the XML for the type.
+	 * 
+	 * @return string
+	 * 
+	 * @todo remove preg_replace() once LIBXML_NOXMLDECL is working properly
+	 * with SimpleXML Element
+	 * @see UpsType_Address::toElement()
+	 */
+	public function __toString() {
+		return preg_replace('/<\?xml.*?\?>\s*/', null, $this->toElement()->asXML());
+	} // end function __toString()
+	
+	/**
+	 * Sets array of service options
+	 * 
+	 * @param array $value
+	 */
+	public function setServiceOptions($value) {
+		$this->options = $value;
+		
+		return true;
+	} // end function setServiceOptions()
+	
+	/**
+	 * Adds a single service option to the array of service options
+	 * 
+	 * @param UpsType_PackageServiceOptions_* $value
+	 */
+	public function setWeight($value) {
+		$this->options[] = $value;
+		
+		return true;
+	} // end function setWeight()
+	
+	/**
+	 * Returns a SimpleXML Element for the type
+	 * 
+	 * @return SimpleXMLElement
+	 * 
+	 * @todo look into why LIBXML_NOBLANKS is not working so we can get rid of
+	 * all these conditionals
+	 * @todo add LIBXML_NOXMLDECL as an option once it's supported
+	 */
+	public function toElement() {
+		$xml = new SimpleXMLElement('<PackageServiceOptions />');
+		
+		// check to see if we have a value for the unit of measurement
+		if (isset($this->units)) {
+			$xml->UnitOfMeasurement->Code = $this->units;
+			
+			// determine the description
+			$description = 'Pounds';
+			if ($this->units == self::UNITS_KILOGRAMS) {
+				$description = 'Kilograms';
+			} // end if the units is centimeters
+			
+			$xml->UnitOfMeasurement->Description = $description;
+		} // end if we have a value for the unit of measurement
+		
+		// check to see if we have a value for the weight
+		if (isset($this->weight)) {
+			$xml->Weight = $this->weight;
+		} // end if we have a value for the weight
+		
+		return $xml;
+	} // end function toElement()
+} // end class UpsType_PackageServiceOptions
---
+++ branches/0.x/UpsType/PackageServiceOptions/COD.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * UPS Address Type.  Makes handling of the COD package service option
+ * easier.
+ * 
+ * Copyright (c) 2008, James I. Armes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+
+/**
+ * UPS Address Type.  Makes handling of the COD package service option
+ * easier.
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ * @package php_ups_api
+ */
+class UpsType_PackageServiceOptions_COD extends UpsType_PackageServiceOptions {
+	/**
+	 * IATA currency code as defined by ISO 4217
+	 * 
+	 * @var string
+	 * @link http://en.wikipedia.org/wiki/ISO_4217#Active_codes
+	 */
+	protected $currency;
+	
+	/**
+	 * The type of funds to be used for the COD payment
+	 * 
+	 * @var integer
+	 */
+	protected $funds_code;
+	
+	/**
+	 * Insured value
+	 * 
+	 * @var double
+	 */
+	protected $value;
+	
+	/**
+	 * Funds code for cash
+	 * 
+	 * @var integer
+	 */
+	const FUNDS_CODE_CASH = 0;
+	
+	/**
+	 * Funds code for check, cashiers check or money order
+	 * 
+	 * @var integer
+	 */
+	const FUNDS_CODE_CHECK_OR_MO = 8;
+	
+	/**
+	 * Constructor. Values may be passed here or by calling the individual set
+	 * functions.
+	 * 
+	 * @param integer $funds_code
+	 * @param string $currency
+	 * @param double $value
+	 */
+	public function __construct($funds_code = null, $currency = null,
+		$value = null) {
+		// set any of the values that were passed in
+		$this->funds_code = $funds_code;
+		$this->currency = $currency;
+		$this->value = $value;
+	} // end function __construct()
+	
+	/**
+	 * Returns the XML for the type.
+	 * 
+	 * @return string
+	 * 
+	 * @todo remove preg_replace() once LIBXML_NOXMLDECL is working properly
+	 * with SimpleXML Element
+	 * @see UpsType_Address::toElement()
+	 */
+	public function __toString() {
+		return preg_replace('/<\?xml.*?\?>\s*/', null, $this->toElement()->asXML());
+	} // end function __toString()
+	
+	/**
+	 * Sets the currency code
+	 * 
+	 * @param string $value
+	 */
+	public function setCurrencyCode($value) {
+		$this->currency = $value;
+		
+		return true;
+	} // end function setCurrencyCode()
+	
+	/**
+	 * Sets the funds code
+	 * 
+	 * @param integer $value
+	 */
+	public function setFundsCode($value) {
+		$this->funds_code = $value;
+		
+		return true;
+	} // end function setFundsCode()
+	
+	/**
+	 * Sets the monetary value
+	 * 
+	 * @param double $value
+	 * @link http://en.wikipedia.org/wiki/ISO_4217#Active_codes
+	 */
+	public function setMonetaryValue($value) {
+		$this->value = $value;
+		
+		return true;
+	} // end function setMonetaryValue()
+	
+	/**
+	 * Returns a SimpleXML Element for the type
+	 * 
+	 * @return SimpleXMLElement
+	 * 
+	 * @todo look into why LIBXML_NOBLANKS is not working so we can get rid of
+	 * all these conditionals
+	 * @todo add LIBXML_NOXMLDECL as an option once it's supported
+	 */
+	public function toElement() {
+		$xml = new SimpleXMLElement('<COD />');
+		
+		// check to see if we have a value for the funds code
+		if (isset($this->funds_code)) {
+			$xml->CODFundsCode = $this->funds_code;
+		} // end if we have a value for the funds code
+		
+		// check to see if we have a value for the currency
+		if (isset($this->currency)) {
+			$xml->CODAmount->CurrencyCode = $this->currency;
+		} // end if we have a value for the currency
+		
+		// check to see if we have a value for the value
+		if (isset($this->value)) {
+			$xml->CODAmount->MonetaryValue = $this->value;
+		} // end if we have a value for the value
+		
+		return $xml;
+	} // end function toElement()
+} // end class UpsType_PackageServiceOptions_COD
